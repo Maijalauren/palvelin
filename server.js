732b946594c53ccdf54f2ba2fd4475cc44d2e30f
Maija Lauren <maija.e.lauren@gmail.com>
2024-02-01 14:18:38 +0200

    hifistelyä

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const vaatteet = require('./vaatteet.json')
 const path = require('path')
+const fs = require (`fs`).promises
+
+app.use(cors())
+
+
+app.get(`/api/login`, async (req, res) => {
+    try  {
+
+        const savedPin = await fs.readFile('./pin.txt', 'utf-8')
+
+        res.send(savedPin)
+
+    } catch {
+    
+        res.status(500).send('Internal Server Error')
+    }
+
+})
+
+
+
+
 
 //tehdään polkumääritys frontend kansioon
 const fronttipolku = path.join(__dirname, './frontend')
@@ -22,6 +45,7 @@ app.get('/api', (req, res) => {
 //vaatedatan lähettäminen
     app.get('/api/vaatteet', (req, res) => {
         res.json(vaatteet)
+        // res.status(200).json(vaatteet)
         }
     )
 
